15dab622c80f87ac09b5d37faec3fcb4fdba48e3
daeng98 <daeng9821@gmail.com>
2025-03-20 17:06:25 +0900

    Feat: 필요 UI 추가, 스크립트 생성

---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private UIStatus uIStatus;
+    [SerializeField] private UIInventory uiInven;
+    [SerializeField] private Character character;
+}
